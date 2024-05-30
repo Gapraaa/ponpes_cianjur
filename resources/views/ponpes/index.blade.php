146a28d6e8f4d60981e967358168c59a97c48058
raichan07 <raichandintar@gmail.com>
2024-05-30 09:06:57 +0700

    up

--- resources/views/ponpes/index.blade.php
+++ resources/views/ponpes/index.blade.php
@@ -556,13 +556,13 @@
                 </div>
             </div>
             <!-- Map Section -->
-            <div class="col-md-5 d-flex justify-content-center align-items-center ">
+            <div class="col-md-6 d-flex justify-content-center align-items-center ">
                 <div class="card mb-3" style="width: 24rem;">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2283.6225033548085!2d106.83765918427203!3d-6.6397279109802945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c8bd0490a1f7%3A0xea6548df140cc716!2sGg.%20PD%20Hasan%20Jl.%20Muara%20No.118%2C%20RT.03%2FRW.01%2C%20Tajur%2C%20Kec.%20Bogor%20Tim.%2C%20Kota%20Bogor%2C%20Jawa%20Barat%2016145!5e0!3m2!1sid!2sid!4v1716963815304!5m2!1sid!2sid" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
             <!-- Social Media Section -->
-            <div class="col-md-3 d-flex flex-column align-items-end ">
+            <div class="col-md-2 d-flex flex-column align-items-end ">
                 <ul class="list-unstyled mb-2">
                     <li><i class="fas fa-phone-alt"></i> 081214192627</li>
                 </ul>
