771932f4d24cfed7cdaa6890afd832c7cfca5e43
raichan07 <raichandintar@gmail.com>
2024-05-30 08:58:54 +0700

    done

--- resources/views/paud/index.blade.php
+++ resources/views/paud/index.blade.php
@@ -432,27 +432,33 @@
   <footer class="text-white pt-5 pb-5" style="background-color: #00763a;">
     <div class="container">
         <div class="row pb-3">
-            <div class="col-md-4">
-                <h5>Pondok Pesantren Ar-Risalah Safi’iyah</h5>
-                <p><b>Alamat</b>: Kp.Paseban RT / RW 07 / 01, Desa Hegarmanah,  Kecamatan Sukaluyu,  Kabupaten Cianjur</p>
+            <!-- Text Section -->
+            <div class="col-md-4 d-flex ">
+                <div>
+                    <h5>Pondok Pesantren Ar-Risalah Safi’iyah</h5>
+                    <p><b>Alamat</b>: Kp.Paseban RT / RW 07 / 01, Desa Hegarmanah,  Kecamatan Sukaluyu,  Kabupaten Cianjur</p>
+                </div>
             </div>
-            <div class="col-md-4">
+            <!-- Map Section -->
+            <div class="col-md-5 d-flex justify-content-center align-items-center ">
+                <div class="card mb-3" style="width: 24rem;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2283.6225033548085!2d106.83765918427203!3d-6.6397279109802945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c8bd0490a1f7%3A0xea6548df140cc716!2sGg.%20PD%20Hasan%20Jl.%20Muara%20No.118%2C%20RT.03%2FRW.01%2C%20Tajur%2C%20Kec.%20Bogor%20Tim.%2C%20Kota%20Bogor%2C%20Jawa%20Barat%2016145!5e0!3m2!1sid!2sid!4v1716963815304!5m2!1sid!2sid" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
-            <div class="col-md-4 d-flex flex-column align-items-end">
+            <!-- Social Media Section -->
+            <div class="col-md-3 d-flex flex-column align-items-end ">
                 <ul class="list-unstyled mb-2">
-                    <li><i class="fas fa-phone-alt font-sans"> </i>   081214192627</li>
+                    <li><i class="fas fa-phone-alt"></i> 081214192627</li>
                 </ul>
                 <div class="mx-0">
-                    <a href="#" class="text-white mr-1"><h5 class="fab fa-facebook"></h5></a>
-                    <a href="#" class="text-white mr-1"><h5 class="fab fa-instagram"></h5></a>
-                    <a href="#" class="text-white mr-1"><h5 class="fab fa-whatsapp"></h5></a>
-                    <a href="#" class="text-white mr-1"><h5 class="fab fa-youtube"></h5></a>
+                    <a href="#" class="text-white mr-2"><h5 class="fab fa-facebook"></h5></a>
+                    <a href="#" class="text-white mr-2"><h5 class="fab fa-instagram"></h5></a>
+                    <a href="#" class="text-white mr-2"><h5 class="fab fa-whatsapp"></h5></a>
+                    <a href="#" class="text-white"><h5 class="fab fa-youtube"></h5></a>
                 </div>
             </div>
         </div>
         <hr class="my-3 bg-white">
-        <div class="row">
-        </div>
     </div>
 </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
